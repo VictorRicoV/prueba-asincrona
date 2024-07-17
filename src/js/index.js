1abf49b46b8b3854274fbb6a4ed368713c46f328
Victor <lessricc@gmail.com>
2024-07-17 17:44:34 +0200

    terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+const textElement = document.getElementById('text');
+const phraseElement = document.getElementById('phrase');
+const buttonElement = document.getElementById('button');
+
+const update = data => {
+  const newId = `ADVICE#${data.slip.id}`;
+  textElement.textContent = newId;
+  const newPhrase = data.slip.advice;
+  phraseElement.textContent = newPhrase;
+};
+const getData = async () => {
+  try {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    // console.log(data.slip.advice);
+    update(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+buttonElement.addEventListener('click', getData);
